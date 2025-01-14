841ecf158000864370012e4e1ebdc1bb0afe2550
Daniel Pora <163474826+dpora@users.noreply.github.com>
2025-01-14 15:23:35 -0500

    new thing

--- Lab1/Form1.Designer.cs
+++ Lab1/Form1.Designer.cs
@@ -32,14 +32,17 @@
             trackBar1 = new TrackBar();
             panel1 = new Panel();
             button1 = new Button();
+            trackBar2 = new TrackBar();
             ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar2).BeginInit();
             SuspendLayout();
             // 
             // button2
             // 
-            button2.Location = new Point(164, 33);
+            button2.Location = new Point(131, 26);
+            button2.Margin = new Padding(2, 2, 2, 2);
             button2.Name = "button2";
-            button2.Size = new Size(112, 34);
+            button2.Size = new Size(90, 27);
             button2.TabIndex = 1;
             button2.Text = "button2";
             button2.UseVisualStyleBackColor = true;
@@ -47,44 +50,59 @@
             // trackBar1
             // 
             trackBar1.BackColor = Color.Red;
-            trackBar1.Location = new Point(302, 21);
+            trackBar1.Location = new Point(225, 17);
+            trackBar1.Margin = new Padding(2, 2, 2, 2);
             trackBar1.Name = "trackBar1";
-            trackBar1.Size = new Size(156, 69);
+            trackBar1.Size = new Size(125, 56);
             trackBar1.TabIndex = 2;
             // 
             // panel1
             // 
             panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             panel1.BackColor = SystemColors.ActiveCaption;
-            panel1.Location = new Point(12, 96);
+            panel1.Location = new Point(10, 77);
+            panel1.Margin = new Padding(2, 2, 2, 2);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1185, 484);
+            panel1.Size = new Size(948, 387);
             panel1.TabIndex = 3;
             panel1.MouseDown += panel1_MouseDown;
             panel1.MouseUp += panel1_MouseUp;
             // 
             // button1
             // 
-            button1.Location = new Point(27, 33);
+            button1.Location = new Point(22, 26);
+            button1.Margin = new Padding(2, 2, 2, 2);
             button1.Name = "button1";
-            button1.Size = new Size(112, 34);
+            button1.Size = new Size(90, 27);
             button1.TabIndex = 4;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
             // 
+            // trackBar2
+            // 
+            trackBar2.BackColor = Color.Red;
+            trackBar2.Location = new Point(354, 17);
+            trackBar2.Margin = new Padding(2);
+            trackBar2.Name = "trackBar2";
+            trackBar2.Size = new Size(125, 56);
+            trackBar2.TabIndex = 5;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1209, 592);
+            ClientSize = new Size(967, 474);
+            Controls.Add(trackBar2);
             Controls.Add(button1);
             Controls.Add(panel1);
             Controls.Add(trackBar1);
             Controls.Add(button2);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -94,5 +112,6 @@
         private TrackBar trackBar1;
         private Panel panel1;
         private Button button1;
+        private TrackBar trackBar2;
     }
 }
